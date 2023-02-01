3ab7974dbe4d5cb4d8a4398fcd760108119df89d
VincentChisaka <vincentchisaka2@gmail.com>
2023-02-01 14:02:28 +0300

    adding the navbar js file

--- navbar/src/App.js
+++ navbar/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Navbar from "./components/Navbar.js"
+
 
 function App() {
   return (
     <div className="App">
-       <h1>yooh</h1>
+       <Navbar/>
     </div>
   );
 }
